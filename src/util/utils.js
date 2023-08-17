9b4474155dbbee3ecb877617df7472ea45c4e094
Grinwitch <grincov.a2001@gmail.com>
2023-08-17 11:45:05 +0600

    Tracking add

--- src/util/utils.js
+++
@@ -1,9 +0,0 @@
-export function priceStyle(price){
-	return (price) ? (price).toLocaleString('ru') : 0;
-}
-
-export const percentCalculate = (num, percent) => {
-    return num - ((num / 100) * percent)
-}
-
-export default {priceStyle, percentCalculate};
